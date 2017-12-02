29f096ed3e895ef271c2df9a85f4b7a6eb18caf2
Andrea Zaffonato <zaffotech@gmail.com>
2017-12-02 16:58:38 +0100

    Palla che salta in campo verde

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    public LevelManager levelManager;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        levelManager = GetComponent<LevelManager>();
+
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        levelManager.LevelSetup();
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jumpHeight;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = new Vector3(0, 10 * jumpHeight * Time.deltaTime, 0);
+        }
+    }
+}
