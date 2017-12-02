b83149eb389b5bb5a8f2f499f3de3e6d191cf233
matteo sumberaz <matteo.sumberaz@glispamedia.com>
2017-12-03 00:32:15 +0100

    bazzillion stuff

---
+++ Assets/screenLimit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenLimit : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+				
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "ground")
+		{
+			//TODO remove
+			Debug.Log("create new ground");
+			GameManager.instance.InstanceNewGround();	
+		} else if (other.gameObject.tag == "player")
+		{
+			//TODO remove
+			Debug.Log("Player out of screen");
+			GameManager.instance.PlayerKilled();
+		}
+		
+	}
+}
