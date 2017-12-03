c21c68a1407f27128d822aa0e763292d7c927b1a
Andrea Zaffonato <zaffotech@gmail.com>
2017-12-03 18:39:33 +0100

    Animazione donna braccio manda al gioco

--- Assets/AnimationControllerScript.cs
+++ Assets/AnimationControllerScript.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AnimationControllerScript : MonoBehaviour {
 
 	public void PermitDialog()
     {
         DialogSceneManager.instance.PermitDialog();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("main");
     }
 }
